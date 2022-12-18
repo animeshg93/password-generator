678cd98974408b39f9d93d003916d54a40677db5
Animesh Gandhi <animeshg93@gmail.com>
2022-12-18 14:57:52 -0700

    Used tuple to better interact with answers

--- quiz.py
+++ quiz.py
@@ -1,29 +1,23 @@
 
 score = 0
 
-def getAnswer(a,b,c,d, correct):
-	print(f"A: {a}\nB: {b}\nC: {c}\nD: {d}")
-	answer = input("\nYour answer: ")
-	option = ''
-	match answer.casefold():
-		case "a":
-			print(f'You chose {a}\n')
-			option = a
-		case "b":
-			print(f'You chose {b}\n')
-			option = b
-		case "c":
-			print(f'You chose {c}\n')
-			option = c
-		case "d":
-			print(f'You chose {d}\n')
-			option = d
+def creatChoiceDict(a,b,c,d):
+	dict = {'a':a,'b':b,'c':c,'d':d}
+	return dict
 
-	if option == correct:
+def getAnswer(choiceDict, correct):
+	for key in choiceDict:
+		print(f'{key.upper()}: {choiceDict[key]}')
+
+	answer = input("\nYour answer: ")
+
+	print(f"You chose {choiceDict[answer.lower()]}\n")
+
+	if answer.lower() == correct:
 		print("You are correct!\n")
 		return True
 	else:
-		print(f"You are incorrect. The correct answer is {correct}\n")
+		print(f"You are incorrect. The correct answer is {choiceDict[correct]}\n")
 		return False
 
 def printScore():
@@ -31,6 +25,7 @@ def printScore():
 	print(dec)
 	print(f"SCORE: {score}")
 	print(dec)
+	print('\n')
 
 def adjustScore(answer):
 	global score
@@ -42,29 +37,20 @@ def adjustScore(answer):
 
 def q1():
 	print('Who is the director of "Mad Men"?')
-	a = "Jake"
-	b = "Will Smith"
-	c = "Jon Hamm"
-	d = "Leo Caprio"
-	answer = getAnswer(a,b,c,d,c)
+	choiceDict = creatChoiceDict('Jake', 'Will', 'Hamm', 'Leo')
+	answer = getAnswer(choiceDict, 'c')
 	adjustScore(answer)
 
 def q2():
 	print('Who started Honda?')
-	a = "Honda Soichiro"
-	b = "Seiji Kuraishi"
-	c = "Toshihiro Mibe"
-	d = "Jackie Chan"
-	answer = getAnswer(a,b,c,d,a)
+	choiceDict = creatChoiceDict('Honda', 'Seiji', 'Mibe', 'Chan')
+	answer = getAnswer(choiceDict, 'a')
 	adjustScore(answer)	
 
 def q3():
 	print('What is the oldest volcano?')
-	a = "Vesuvius"
-	b = "Etna"
-	c = "Fuji"
-	d = "Pompey"
-	answer = getAnswer(a,b,c,d,b)
+	choiceDict = creatChoiceDict('Vesuvius', 'Etna', 'Pompey', 'Fuji')
+	answer = getAnswer(choiceDict,'b')
 	adjustScore(answer)
 	
 q1()
