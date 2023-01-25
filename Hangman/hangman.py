4cbe98efa7b61539c8fe0fc4fdfeb89375bdf955
gandhia <animesh.gandhi@earlywarning.com>
2023-01-25 16:49:55 -0700

    Added Hangman code

---
+++ Hangman/hangman.py
@@ -0,0 +1,59 @@
+import random
+from hangmanPics import HANGMAN_PICS
+
+with open("words.txt", 'r') as textfile:
+    words = textfile.read().splitlines()
+
+
+def createWordFreqMap(word):
+    dict = {}
+    index = 0
+    for letter in word:
+        if dict.get(letter) is None:
+            indexList = [index]
+            dict[letter] = indexList
+        else:
+            dict.get(letter).append(index)
+        index += 1
+    return dict
+
+
+def gameLogic(word, guess, freqMap, guessed_letters):
+    hidden = "-" * len(word)
+    while guess < len(HANGMAN_PICS) - 1 and '-' in hidden:
+        print(HANGMAN_PICS[guess])
+        print(f"The word for you to guess is {hidden}")
+        letter = input("Please guess your letter: ").lower()
+
+        while letter in guessed_letters:
+            letter = input("That letter has been already guessed. Please try again: ").lower()
+
+        if letter in freqMap:
+            indices = freqMap.get(letter)
+            hiddenList = list(hidden)
+            for index in indices:
+                hiddenList[index] = letter
+            hidden = "".join(hiddenList)
+        else:
+            guess += 1
+            guessed_letters.append(letter)
+
+    if guess < len(HANGMAN_PICS) - 1:
+        print(f"Congratulations, you guessed the right word. The word was \"{word}\"")
+    else:
+        print(HANGMAN_PICS[guess])
+        print(f"Sorry, the correct word was {word}")
+
+
+def startGame():
+    word = "".join(random.choice(words)).lower()
+    guessed_letters = []
+    guess = 0
+    # print(word)
+    freqMap = createWordFreqMap(word)
+
+    print("Hello, welcome to Hangman.")
+    gameLogic(word, guess, freqMap, guessed_letters)
+
+
+startGame()
--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -8,7 +8,7 @@ def playerMove(player):
     square = int(input("Please enter the square number for your move, from 0-8: "))
     populateBoard(square, player)
     if winLogic(square, player):
-        print("The player has won!")
+        print("\nThe player has won!")
         return True
 
 
@@ -16,7 +16,7 @@ def computerMove(computer):
     square = int(input("Please enter the square number for computer move, from 0-8: "))
     populateBoard(square, computer)
     if winLogic(square, computer):
-        print("The computer has won!")
+        print("\nThe computer has won!")
         return True
 
 
@@ -24,8 +24,7 @@ def playGame(player, computer):
     global counter
     over = False
     while counter < 8 and not over:
-        if playerMove(player) or computerMove(computer):
-            over = True
+        over = playerMove(player) or computerMove(computer)
         counter += 2
 
     # calling the computer function for one last move by the computer
@@ -37,7 +36,7 @@ def game():
     player = input("Welcome to Tic Tac Toe. Please choose your input type, 'X' or 'O': ").upper()
     print(f"You have chosen {player}.")
     computer = 'O' if player == 'X' else 'X'
-
+    printBoard()
     playGame(player, computer)
 
 
